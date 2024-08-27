d43a27dbb1ee886da1a93016f27f95593007e176
John Benninghoff <jbenninghoff@mac.com>
2024-08-27 14:36:03 -0500

    Revert "Add Example Report Vignette"
    This reverts commit a6909ca17d93de45dc6b907177fa20bd2ae1aed4.

--- data-raw/widget.R
+++
@@ -1,7 +0,0 @@
-# load example data from Excel
-library(readxl)
-
-widget_risks <- read_xlsx("data-raw/widget.xlsx", sheet = "Risks")
-widget_estimates <- read_xlsx("data-raw/widget.xlsx", sheet = "Estimates")
-
-usethis::use_data(widget_risks, widget_estimates, overwrite = TRUE, version = 3)
