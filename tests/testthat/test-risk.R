1342249d0c2cb62b3ebf3ddd5664188955db39d1
John Benninghoff <jbenninghoff@mac.com>
2025-08-18 13:34:08 -0500

    Add treatment parameter to calc_risk()

--- tests/testthat/test-risk.R
+++ tests/testthat/test-risk.R
@@ -12,10 +12,12 @@ test_that("lnorm_param returns correct values", {
 
 test_that("calc_risk returns correct values", {
   test_calc <- withr::with_seed(
-    429406278, calc_risk("Loss of Customer", 2.0, 14.162084, 1.610574, runs = 10)
+    429406278,
+    calc_risk("Loss of Customer", 2.0, 14.162084, 1.610574, treatment = "base", runs = 10)
   )
   expected_years <- 1:10
   expected_risks <- rep("Loss of Customer", 10)
+  expected_treatment <- rep("base", 10)
   expected_events <- c(2L, 4L, 1L, 1L, 1L, 1L, 1L, 3L, 2L, 2L)
   expected_losses <- c(
     4907733.51446033, 13378340.3529084, 428313.785080057, 361303.191424744, 3128642.89100821,
@@ -24,6 +26,7 @@ test_that("calc_risk returns correct values", {
 
   expect_identical(test_calc$year, expected_years)
   expect_identical(test_calc$risk, expected_risks)
+  expect_identical(test_calc$treatment, expected_treatment)
   expect_identical(test_calc$events, expected_events)
   expect_equal(test_calc$losses, expected_losses, tolerance = testthat_tolerance())
 })
